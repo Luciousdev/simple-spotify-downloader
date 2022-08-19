3f9826a93d8dd0d339bfd831866772cdef52741d
Luciousdev <jochemem01@gmail.com>
2022-08-19 19:28:59 +0200

    added time

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pyfiglet
 import subprocess
 from datetime import datetime
+import pytz
 
 now = datetime.now()
 # traverse the info
@@ -16,6 +17,9 @@ for i in new:
 
 ascii_banner = pyfiglet.figlet_format("Spotify Downloader")
 print(ascii_banner)
+tz_London = pytz.timezone('Europe/London')
+datetime_London = datetime.now(tz_London)
+print("London time:", datetime_London.strftime("%H:%M:%S"))
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 song = input("URL: ")
