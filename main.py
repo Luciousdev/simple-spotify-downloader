24c5509e913409b9337f949e8168d24bd3026aa8
Luciousdev <jochemem01@gmail.com>
2022-08-19 18:03:47 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import os
 import pyfiglet
+import subprocess
+from datetime import datetime
+
+now = datetime.now()
+# traverse the info
+Id = subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')
+new = []
+ 
+# arrange the string into clear info
+for item in Id:
+    new.append(str(item.split("\r")[:-1]))
+for i in new:
+    print(i[2:-2])
 
 ascii_banner = pyfiglet.figlet_format("Spotify Downloader")
 print(ascii_banner)
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
 song = input("URL: ")
 os.system(f"spotdl {song}")
 os.remove(".spotdl-cache")
